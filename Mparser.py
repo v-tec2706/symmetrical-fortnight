b3c115dd0b8d25ee971ff6927dc1d635b30ade50
v-tec2706 <witus2706@gmail.com>
2019-03-26 23:15:17 +0100

    bugfix

--- Mparser.py
+++ Mparser.py
@@ -3,15 +3,17 @@
 import scanner
 import ply.yacc as yacc
 
+
 tokens = scanner.tokens
 
+
 precedence = (
-    ('nonassoc', 'IF'),
-    ('nonassoc', 'ELSE'),
-    ('right', '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
-    ('nonassoc', 'LEEQ', 'GREQ', 'NOTEQ', 'EQ'),
-    ("left", 'DOTADD', 'DOTSUB'),
-    ("left", 'DOTMUL', 'DOTDIV'),
+   ('nonassoc', 'IF'),
+   ('nonassoc', 'ELSE'),
+   ('right', '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
+   ('nonassoc', 'LEEQ', 'GREQ', 'NOTEQ', 'EQ'),
+   ("left", 'DOTADD', 'DOTSUB'),
+   ("left", 'DOTMUL', 'DOTDIV'),
 )
 
 
@@ -24,132 +26,49 @@ def p_error(p):
         print("Unexpected end of input")
 
 
-# wyrazenia
-
-
-# inicjalizacje
-
-
-# instrukcje
 def p_start(p):
     """start : expression
-             | blockOfExpressions
-             | start blockOfExpressions
              | start expression"""
-
 
 def p_expression(p):
     """expression : initialization
-                  | construction
-                  | simple_func
-                  """
-
-
-def p_simple_func(p):
-    """simple_func : PRINT to_print ';'
-                   | return ';'
-                   | CONTINUE ';'
-                   | BREAK ';'"""
-
-def p_return(p):
-    """return : RETURN comparison"""
-
-
-def p_toPrint(p):
-    """to_print : content
-                | content ',' to_print
-    """
-
-def p_content(p):
-    """content : STRING
-               | var
-    """
-
-def p_condition(p):
-    """condition : comparison '<' comparison
-                 | comparison '>' comparison
-                 | comparison EQ comparison
-                 | comparison LEEQ comparison
-                 | comparison GREQ comparison
-                 | comparison NOTEQ comparison"""
-
-def p_comparison(p):
-    """comparison : var
-                  | operation"""
-
-
-def p_construction(p):
-    """construction : ifConstr
-                    | whileConstr
-                    | forConstr"""
-
-
-def p_whileConstr(p):
-    """whileConstr : WHILE '(' condition ')' blockOfExpressions
-    """
-
-
-
-
-
-
-def p_ifConstr(p):
-     """ifConstr : singleIf
-                 | singleIf else
-                 | singleIf elseIfs
-                 """
-
-
-def p_elseIfs(p):
-    """elseIfs : ELSE IF '(' condition ')' blockOfExpressions
-               | ELSE IF '(' condition ')' blockOfExpressions elseIfs
-               | ELSE IF '(' condition ')' blockOfExpressions else
-    """
-
-def p_else(p):
-    """else : ELSE blockOfExpressions"""
-
-def p_singleIf(p):
-    """singleIf : IF '(' condition ')' blockOfExpressions"""
-
-
-
-
-
-
-
-
-
-def p_for(p):
-    """forConstr : FOR ID '=' var ':' var blockOfExpressions"""
-
-def p_var(p):
-    """var : number
-           | ID"""
-
-
-def p_blockOfExpressions(p):
-    """blockOfExpressions : expression
-                          | '{' expressions '}'"""
-
-
-def p_expressions(p):
-    """expressions : expression
-                   | expression expressions"""
-
+                  | assign"""
 
 # initialization
 def p_init_num_str(p):
     """initialization : ID '=' number ';'
                       | ID '=' STRING ';'"""
 
+def p_init_operation(p):
+    """initialization : ID '=' operation ';'"""
 
-# p[0] = p[1]
+def p_operation_sum(p):
+    """operation : var '+' var
+                 | var '-' var"""
 
+
+def p_operation_dot_sum(p):
+    """operation : var DOTADD var
+                 | var DOTSUB var
+                 | var DOTMUL var
+                 | var DOTDIV var"""
+
+
+def p_operation_mul(p):
+    """operation : var '*' var
+                 | var '/' var"""
+
+
+def p_var(p):
+    """var : number
+           | ID """
+
+def p_unary_op(p):
+    """operation : '-' var
+                 | var "\'" """
 
 def p_init_funct(p):
     """initialization : ID '=' special_func ';'"""
-
 
 def p_spec_fun(p):
     """special_func : ZEROS '(' INTNUM ')'
@@ -160,7 +79,6 @@ def p_spec_fun(p):
 def p_init_elems(p):
     """initialization : ID '=' matrix ';'
                       | exact_pos '=' number ';'"""
-
 
 def p_exact_pos(p):
     """exact_pos : ID '[' INTNUM ',' INTNUM ']' """
@@ -175,24 +93,6 @@ def p_matrix(p):
     """matrix : '[' elements ']'
               | '[' matrix ']'"""
 
-def p_init_operation(p):
-    """initialization : ID '=' operation ';'"""
-
-
-
-def p_operation(p):
-    """operation : var '+' var
-                 | var '-' var
-                 | var '*' var
-                 | var '/' var
-    """
-
-def p_dot_operation(p):
-    """operation : var DOTADD var
-                 | var DOTSUB var
-                 | var DOTMUL var
-                 | var DOTDIV var
-    """
 
 def p_element(p):
     """elements : number ',' elements
@@ -200,12 +100,148 @@ def p_element(p):
                 | number
                 | empty"""
 
+def p_assign(p):
+    """assign : ID ADDASSIGN var ';'
+              | ID SUBASSIGN var ';'
+              | ID MULASSIGN var ';'
+              | ID DIVASSIGN var ';'"""
 
+
+def p_condition(p):
+    """condition : var '<' var
+                 | var '>' var
+                 | var EQ var
+                 | var LEEQ var
+                 | var GREQ var
+                 | var NOTEQ var"""
 def p_empty(p):
     'empty :'
     pass
 
+parser = yacc.yacc()
+
+
+#!/usr/bin/python
+
+import scanner
+import ply.yacc as yacc
+
+
+tokens = scanner.tokens
+
+
+precedence = (
+   ('nonassoc', 'IF'),
+   ('nonassoc', 'ELSE'),
+   ('right', '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
+   ('nonassoc', 'LEEQ', 'GREQ', 'NOTEQ', 'EQ'),
+   ("left", 'DOTADD', 'DOTSUB'),
+   ("left", 'DOTMUL', 'DOTDIV'),
+)
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')"
+              .format(p.lineno, scanner.find_column(p.lexer.lexdata, p),
+                      p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+def p_start(p):
+    """start : expression
+             | start expression"""
+
+def p_expression(p):
+    """expression : initialization
+                  | assign"""
+
+# initialization
+def p_init_num_str(p):
+    """initialization : ID '=' number ';'
+                      | ID '=' STRING ';'"""
+
+def p_init_operation(p):
+    """initialization : ID '=' operation ';'"""
+
+def p_operation_sum(p):
+    """operation : var '+' var
+                 | var '-' var"""
+
+
+def p_operation_dot_sum(p):
+    """operation : var DOTADD var
+                 | var DOTSUB var
+                 | var DOTMUL var
+                 | var DOTDIV var"""
+
+
+def p_operation_mul(p):
+    """operation : var '*' var
+                 | var '/' var"""
+
+
+def p_var(p):
+    """var : number
+           | ID """
+
+def p_unary_op(p):
+    """operation : '-' var
+                 | var "\'" """
+
+def p_init_funct(p):
+    """initialization : ID '=' special_func ';'"""
+
+def p_spec_fun(p):
+    """special_func : ZEROS '(' INTNUM ')'
+                    | ONES '(' INTNUM ')'
+                    | EYE '(' INTNUM ')'"""
+
+
+def p_init_elems(p):
+    """initialization : ID '=' matrix ';'
+                      | exact_pos '=' number ';'"""
+
+def p_exact_pos(p):
+    """exact_pos : ID '[' INTNUM ',' INTNUM ']' """
+
+
+def p_number(p):
+    """number : INTNUM
+              | FLOATNUM"""
+
+
+def p_matrix(p):
+    """matrix : '[' elements ']'
+              | '[' matrix ']'"""
+
+
+def p_element(p):
+    """elements : number ',' elements
+                | number ';' elements
+                | number
+                | empty"""
+
+def p_assign(p):
+    """assign : ID ADDASSIGN var ';'
+              | ID SUBASSIGN var ';'
+              | ID MULASSIGN var ';'
+              | ID DIVASSIGN var ';'"""
+
+
+def p_condition(p):
+    """condition : var '<' var
+                 | var '>' var
+                 | var EQ var
+                 | var LEEQ var
+                 | var GREQ var
+                 | var NOTEQ var"""
+def p_empty(p):
+    'empty :'
+    pass
 
 parser = yacc.yacc()
+
 
 
